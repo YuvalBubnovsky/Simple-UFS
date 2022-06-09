e29a4ef98d557a5718ff8fc12bdb86638b4303bc
YuvalBubnovsky <yuval.bubnov@gmail.com>
2022-06-09 12:18:10 +0300

    basic implementation

--- myfs.c
+++ myfs.c
@@ -1,3 +1,100 @@
 #include "myfs.h"
 
-// TODO:
+struct superblock sb;
+struct inode* inodes;
+struct block* blocks;
+
+void mkfs(){
+    sb.num_of_inodes = 100;
+    sb.num_of_blocks = 1000;
+    sb.size_of_blocks = sizeof(struct block);
+
+    // init inodes
+    inodes = malloc(sb.num_of_inodes * sizeof(struct inode));
+    for(int i=0; i<sb.num_of_inodes; i++){
+        inodes[i].first_block = 0;
+        inodes[i].size = 0;
+        inodes[i].name[0] = '\0';
+    }
+
+    // init blocks
+    blocks = malloc(sb.num_of_blocks * sizeof(struct block));
+    for(int i=0; i<sb.num_of_blocks; i++){
+        blocks[i].next = 0;
+        blocks[i].data[0] = '\0';
+    }
+
+
+}
+
+void mysync(){
+    FILE *file;
+    file = fopen("myfs_data", "w+");
+
+    // write superblock
+    fwrite(&sb, sizeof(struct superblock), 1, file);
+
+    // write inodes
+    for (int i=0; i<sb.num_of_inodes; i++){
+        fwrite(&inodes[i], sizeof(struct inode), 1, file);
+    }
+
+    // write blocks
+    for (int i=0; i<sb.num_of_blocks; i++){
+        fwrite(&blocks[i], sizeof(struct block), 1, file);
+    }
+    fclose(file);
+
+}
+
+int mymount(const char *source, const char *target, const char *filesystemtype, unsigned long mountflags, const void *data){
+    FILE *file;
+    // TODO: take source argument and create the file-system (and file) in the target path
+    file = fopen("myfs_data", "r");
+
+    // read superblock
+    fread(&sb, sizeof(struct superblock), 1, file);
+
+    //read inodes
+    inodes = malloc(sb.num_of_inodes * sizeof(struct inode));
+    fread(inodes, sizeof (struct inode), sb.num_of_inodes, file);
+    
+
+   /* for(int i=0; i<sb.num_of_inodes; i++){
+        fread(&inodes[i], sizeof(struct inode), 1, file);
+    }*/
+
+    //read blocks
+    blocks = malloc(sb.num_of_blocks * sizeof(struct block));
+    fread(blocks, sizeof (struct block), sb.num_of_blocks, file);
+
+    /*for(int i=0; i<sb.num_of_blocks; i++){
+        fread(&blocks[i], sizeof(struct block), 1, file);
+    }*/
+    
+    fclose(file);
+}
+int myopen(const char *pathname, int flags){
+
+}
+int myclose(int myfd){
+
+}
+ssize_t myread(int myfd, void *buf, size_t count){
+
+}
+ssize_t mywrite(int myfd, const void *buf, size_t count){
+
+}
+off_t mylseek(int myfd, off_t offset, int whence){
+
+}
+struct myDIR *myopendir(const char *name){
+
+}
+struct mydirent *myreaddir(struct myDIR *dirp){
+
+}
+int myclosedir(struct myDIR *dirp){
+
+}
--- myfs.h
+++ myfs.h
@@ -3,6 +3,8 @@
 
 #pragma once
 #include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
 #include <aio.h> // contains ssize_t and off_t
 
 struct superblock
@@ -14,7 +16,7 @@ struct superblock
 
 struct inode
 {
-    unsigned int first_block;
+    unsigned int first_block; // address of first block
     size_t size;
     char name[8];
 };
@@ -22,9 +24,10 @@ struct inode
 
 struct block
 {
-    unsigned int next;
+    unsigned int next; // address of next block
     char data[512]; //512 was historically used in some version of linux, source: https://www.youtube.com/watch?v=sLR17lUjTpc&ab_channel=C%2FC%2B%2BDublinUserGroup (lecture, not implementation)
 };
+
 
 struct myopenfile
 {
@@ -39,26 +42,30 @@ struct myopenfile
      */
 };
 
-struct mydirent
+struct mydirent 
 {
-    int name[8];
+    int d_name[8];
     int fd[20];
 };
 
 struct myDIR
 {
-    int temp;
+    struct mydirent ent;
     // TODO
 };
 
+// https://man7.org/linux/man-pages/man0/dirent.h.0p.html
+
+void mkfs();
+void mysync();
 int mymount(const char *source, const char *target, const char *filesystemtype, unsigned long mountflags, const void *data);
 int myopen(const char *pathname, int flags);
 int myclose(int myfd);
 ssize_t myread(int myfd, void *buf, size_t count);
 ssize_t mywrite(int myfd, const void *buf, size_t count);
 off_t mylseek(int myfd, off_t offset, int whence);
-myDIR *myopendir(const char *name);
-struct mydirent *myreaddir(myDIR *dirp);
-int myclosedir(myDIR *dirp);
+struct myDIR *myopendir(const char *name);
+struct mydirent *myreaddir(struct myDIR *dirp);
+int myclosedir(struct myDIR *dirp);
 
 #endif
